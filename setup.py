8626f47a7082f5d63405c72781fef1f105228615
daizhaolin <>
2020-02-06 19:51:21 +0800

    Fixed setup.py py_modules

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='piplus',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/daizhaolin/piplus',
     license='BSD',
     author='daizhaolin',
     author_email='',
     description='Python Package Installer Plus',
     long_description=__doc__,
+    py_modules=['piplus'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
